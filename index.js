98ac051aeb76dc6d595484280824ce5882ed58c2
mdyeamin <ahmedyeamin05@gmail.com>
2026-05-12 10:56:51 +0600

    get api by single id

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 app.use(cors());
-app.use(express.json())
-const port = process.env.PORT
+app.use(express.json());
+const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
@@ -21,18 +21,33 @@ async function run() {
     await client.connect();
     const db = client.db("venture_vista");
     const destinationCollection = db.collection("destinations");
+
     // *****
     // get all data from the collection
-    app.get("/", (req, res) => {
-      res.send("Hello World!");
+    app.get("/destinations", async (req, res) => {
+      const result = await destinationCollection.find().toArray();
+      res.send(result);
     });
+
+    // get single  data by id
+
+    app.get("/destinations/:id", async (req, res) => {
+      const id = req.params.id;
+      const findOne = await destinationCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(findOne);
+    });
+
     // post data to the collection
-    app.post("/destination", async(req, res) => {
-        const destination = req.body;
-        const result = await destinationCollection.insertOne(destination)
-        console.log("new post added",result);
-        res.send(result)
-        
+    app.post("/destinations", async (req, res) => {
+      const destination = req.body;
+      console.log(destination, "desti");
+
+      const result = await destinationCollection.insertOne(destination);
+      console.log("new post added", result);
+      res.send(result);
     });
 
     // *****
